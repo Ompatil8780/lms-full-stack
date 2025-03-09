a7db12c6e55b93954971021b379dfec7191916d4
Om Patil <Ompatil8780@gmail.com>
2025-03-09 23:06:58 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -27,7 +27,7 @@ export const AppContextProvider = (props) => {
 
         try {
 
-            const { data } = await axios.get(backendUrl + '/api/course/all');
+            const { data } = await axios.get('/api/course/all');
 
             if (data.success) {
                 setAllCourses(data.courses)
